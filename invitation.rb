90c4fdeb934721bc72a2426a98e5666646de1dcc
Elizabeth Chan <lmchan@brynmawr.edu>
2018-06-29 19:38:45 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -3,6 +3,18 @@
 # Try starting out with puts'ing a string.
 
 
+puts "guest_name?"
+guest_name = gets.chomp.capitalize
+puts "party_name?"
+party_name = gets.chomp.capitalize
+puts "date?"
+date = gets.chomp.capitalize
+puts "time?"
+time = gets.chomp
+puts "host_name?"
+host_name = gets.chomp.capitalize
+
+
 puts
 "Dear #{guest_name},
 
